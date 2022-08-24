c3a6b1d0892705ca6bdb1623b0fc67da518eafc6
Alex <aileenferesp@gmail.com>
2022-08-24 09:50:40 -0500

    consulta a quotes funcionando

---
+++ codigo.js
@@ -0,0 +1,98 @@
+const apiUrl = "https://breakingbadapi.com/api/"
+
+/*const fakeQuotes = [
+    {
+        //ctrl D para seleccionar misma cosa multiple times, ctrl P para buscar extension con >, ctrl alt i para numerar 
+        quote: "turip IP IP IP TUrip",
+        author:"Authors Name  1",
+        id: 1
+    },
+    {
+        quote: "Culpa sunt laborum mollit tempor dolor amet ex ipsum.",
+        author:"Authors Name  2",
+        id: 2
+    },
+    {
+        quote: "Minim aliquip irure quis proident deserunt dolor minim ipsum in.",
+        author:"Authors Name  3",
+        id: 3
+    },
+    {
+        quote: "Non fugiat nisi tempor ipsum nostrud fugiat non laborum eu reprehenderit commodo.",
+        author:"Authors Name  4",
+        id: 4
+    },
+    {
+        quote: "Sint in ipsum quis fugiat voluptate ad officia sunt mollit sunt duis sint sunt.",
+        author:"Authors Name  5",
+        id: 5
+    }
+]*/
+
+function doQuery(url, displayFunction){
+
+//mandamos una solicitud y obtenemos una promesa
+const request = fetch(apiUrl + url)
+
+//esperar a que resuelva la promesa
+request.then(function(response){
+
+    //info sobre nuestra respuesta
+    console.log( "response", response )
+
+    //extraer cuerpo de respuesta
+    response.json().then (function(data){
+        console.log("data", data)
+
+if(typeof displayFunction == "function"){
+    displayFunction(data)
+}
+
+       
+    })
+
+})
+
+console.log("request", request)
+
+}
+
+function displayQuotes(data){
+
+    console.log("display", data);
+
+data.forEach(createAppendQuote)
+}
+
+function createAppendQuote(quote){
+
+    const body = document.querySelector("body")
+
+    const quoteBox = CreateQuoteHTML(quote)
+
+    body.append(quoteBox)
+}
+
+function CreateQuoteHTML(quote){
+
+const quoteBox = document.createElement("blockquote")
+const textBox = document.createElement("p")
+const authorBox = document.createElement("p")
+
+textBox.innerHTML = quote.quote
+authorBox.innerHTML = quote.author
+
+quoteBox.append(textBox)
+quoteBox.append(authorBox)
+
+quoteBox.classList.add("quote")
+
+return quoteBox
+}
+
+doQuery("quotes", displayQuotes)
+doQuery("characters")
+doQuery("episodes")
+
+console.log("Consulta API")
+
